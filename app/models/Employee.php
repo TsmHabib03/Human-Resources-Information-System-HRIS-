ff016e7b7030c57ca0c405eb29e57bd416de2e21
KennDlg <aerichononoa@gmail.com>
2026-04-25 14:48:04 +0800

    debug fix

--- app/models/Employee.php
+++ app/models/Employee.php
@@ -195,8 +195,13 @@ final class Employee extends Model
         $conditions = [];
 
         if ($query !== '') {
-            $conditions[] = '(e.employee_code LIKE :query OR e.first_name LIKE :query OR e.last_name LIKE :query OR e.email LIKE :query OR d.department_name LIKE :query OR ds.designation_name LIKE :query)';
-            $params['query'] = '%' . $query . '%';
+            $conditions[] = '(e.employee_code LIKE :q1 OR e.first_name LIKE :q2 OR e.last_name LIKE :q3 OR e.email LIKE :q4 OR d.department_name LIKE :q5 OR ds.designation_name LIKE :q6)';
+            $params['q1'] = '%' . $query . '%';
+            $params['q2'] = '%' . $query . '%';
+            $params['q3'] = '%' . $query . '%';
+            $params['q4'] = '%' . $query . '%';
+            $params['q5'] = '%' . $query . '%';
+            $params['q6'] = '%' . $query . '%';
         }
 
         if ($status !== '') {
